45edb3fa910f8eb4c2bd80ca0d7e1e17d2735cca
MortezaTavasoly <135455083+MortezaTavasoly@users.noreply.github.com>
2023-11-10 04:37:13 -0800

    Update LocaleContext.tsx

--- src/context/LocaleContext.tsx
+++ src/context/LocaleContext.tsx
@@ -1,19 +1,15 @@
 import { createContext } from "react";
 import { ProviderProps } from "./ThemeContext";
 
-// در این قسمت زبان پیشفرض وارد شده است
 import { language } from "./language";
 
-// این قسمت تعیین کننده ی غالب کانتکست است
 export type LanguageState = {
   locale: string;
   setLocale: () => void;
 };
 
-// در این قسمت کانتکست ساخته میشود و زبان پیشفرض به آن داده میشود
 export const LanguageContext = createContext<LanguageState>(language);
 
-// قسمت پروایدر برای تعیین محدوده ی قابل درسترس بودن یک داده ی گلوبال
 export default function LocaleContext({ children }: ProviderProps) {
   const langState: LanguageState = language;
   return (
